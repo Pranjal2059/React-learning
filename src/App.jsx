c53458c074aea2106653704e7c8f76055847649f
Pranjal2059 <Pranjalsharma2059@gmail.com>
2024-07-09 14:31:24 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './index.css';
+import { AppBar } from './components/navbar';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        <div className="">
+     <AppBar/>
+        <h1 className='text-center'>Heading</h1>
+        </div>
+      </h1>
+    </>
+  );
+}
+
+export default App;
